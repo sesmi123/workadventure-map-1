5fbea37bf288d6e6e750eba57cf403e3a97dea87
Sesmi Athiyarath <sesmi.athiyarath@siemens-healthineers.com>
2022-10-04 15:40:56 +0530

    inital commit

---
+++ shslogo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="shslogo" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="C:/Users/z0047swc/Downloads/shslogo.png" trans="ff00ff" width="279" height="181"/>
+</tileset>
